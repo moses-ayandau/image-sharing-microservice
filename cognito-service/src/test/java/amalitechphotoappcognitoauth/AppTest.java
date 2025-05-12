9cc1c7cae54a5953664e73fa414da8dab8acf144
rose-tetteh <rose.tetteh@amalitech.com>
2025-05-12 17:40:17 +0000

    feat:add feature to send email to user on every sign-up

---
+++ cognito-service/src/test/java/amalitechphotoappcognitoauth/AppTest.java
@@ -0,0 +1,14 @@
+package amalitechphotoappcognitoauth;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+//  @Test
+//  public void dummyTest() {
+//    // This is a placeholder test
+//    assertTrue(true);
+//  }
+}

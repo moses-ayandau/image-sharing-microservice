fbbc7e0955f77663ca8822ab914d77cfbcaf5ae4
nnyabe.chamamme <nnyabe.chamamme@amalitech.com>
2025-05-14 13:17:33 +0000

    Fix: Typo in the files

--- recycle-service/src/test/java/com/handlers/PermanentlyDeleteImageHandlerTest.java
+++ recycle-service/src/test/java/com/handlers/PermanentlyDeleteImageHandlerTest.java
@@ -331,6 +331,7 @@ public class PermanentlyDeleteImageHandlerTest {
         }
     }
 
+
     @Test
     public void testHandleRequest_GetItemFromDynamoException() {
         APIGatewayProxyRequestEvent request = new APIGatewayProxyRequestEvent();
